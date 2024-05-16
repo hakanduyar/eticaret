89f51fe2152db21f4981e6a40d1c15a6965a3298
rikords <iamhakanduyar@gmail.com>
2024-05-16 18:19:37 +0300

    "Updated Category component with new CSS classes and styling"

--- app/components/home/Category.tsx
+++ app/components/home/Category.tsx
@@ -21,9 +21,14 @@ const Category = () => {
     },
   ];
   return (
-    <div>
+    <div className="flex items-center justify-center px-3 md:px-10 gap-3 md:gap-10 py-5 md:py-8 overflow-x-auto">
       {categoryList.map((category, index) => (
-        <div key={index}>{category.name}</div>
+        <div
+          className="border text-slate-500 rounded-xl min-w-[120px] flex  items-center justify-center px-4 py-2 text-center cursor-pointer flex-1"
+          key={index}
+        >
+          {category.name}
+        </div>
       ))}
     </div>
   );
