9647685e1c3c9f6706dff1493c1a21fec143c44d
rikords <iamhakanduyar@gmail.com>
2024-05-18 12:45:10 +0300

    "Added Banner import and component to Home function"

---
+++ app/components/home/Banner.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Banner = () => {
+  return (
+    <div className="h-[237px] bg-orange-500 flex items-center justify-center">
+      <div className="h-[137px] relative w-full">
+        <Image
+          src={"/hepsiburada-da-pesin-fiyatina-2-tak2023-11-01.webp"}
+          fill
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
