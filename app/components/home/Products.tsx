2facacdf3c8b262acf0020c946afa3423f318aa6
rikords <iamhakanduyar@gmail.com>
2024-05-20 07:14:17 +0300

    "Updated Heading component and added product list to Products component"

--- app/components/home/Products.tsx
+++ app/components/home/Products.tsx
@@ -1,9 +1,16 @@
+import { products } from "@/utils/Products";
 import Heading from "../general/Heading";
+import ProductCard from "./ProductCard";
 
 const Products = () => {
   return (
     <div>
       <Heading text="Tüm Ürünler" />
+      <div>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
