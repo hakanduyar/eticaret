2337299ca486b06fcad17079c5954d4465a226ac
rikords <iamhakanduyar@gmail.com>
2024-05-23 20:49:52 +0300

    "Updated ProductCard to use textClip utility.

--- app/components/home/ProductCard.tsx
+++ app/components/home/ProductCard.tsx
@@ -1,16 +1,25 @@
+import textClip from "@/utils/TextClip";
+import { Rating } from "@mui/material";
 import Image from "next/image";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
-    <div className="w-[240px] shadow-lg p-2 rounded-md">
+    <div className="w-[240px] cursor-pointer flex flex-col flex-1 shadow-lg p-2 rounded-md">
       <div className="relative h-[150px]">
         <Image
           src={product.image}
           fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           alt=""
           className="object-contain"
         />
+      </div>
+      <div className="text-center mt-2 space-y-1">
+        <div>{textClip(product.name)}</div>
+        <Rating name="read-only" value={4} readOnly />
+        <div className="text-orange-500 font-bold text-lg md:text-xl">
+          {product.price} ₺
+        </div>
       </div>
     </div>
   );
