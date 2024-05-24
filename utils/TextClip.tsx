ef5835fcca7928bd60294dd36920edf2381fad27
rikords <iamhakanduyar@gmail.com>
2024-05-24 18:20:59 +0300

    update

--- utils/TextClip.tsx
+++ utils/TextClip.tsx
@@ -1,6 +1,6 @@
 const textClip = (text: string) => {
-  if (text.length < 20) return;
-  return text.substring(0, 20) + "...";
+  if (text.length < 15) return text;
+  return text.substring(0, 15) + "...";
 };
 
 export default textClip;
