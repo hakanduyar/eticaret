0321e3d3933c2eb2afbd14b13a6f9b92e15eb561
rikords <iamhakanduyar@gmail.com>
2024-05-28 14:17:18 +0300

    "Updated Footer component and ProductCard component with new styles and rating calculation"

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full h-24 bg-white text-lg md:text-2xl text-orange-500 border-t-2 flex items-center justify-center mt-3 md:mt-10">
+      En son düzenle
+    </div>
+  );
 };
 
 export default Footer;
--- app/components/home/ProductCard.tsx
+++ app/components/home/ProductCard.tsx
@@ -3,6 +3,9 @@ import { Rating } from "@mui/material";
 import Image from "next/image";
 
 const ProductCard = ({ product }: { product: any }) => {
+  let productRating =
+    product?.reviews?.reduce((acc: number, item: any) => acc + item.rating, 0) /
+    product?.reviews?.length;
   return (
     <div className="w-[240px] cursor-pointer flex flex-col flex-1 shadow-lg p-2 rounded-md">
       <div className="relative h-[150px]">
@@ -16,7 +19,7 @@ const ProductCard = ({ product }: { product: any }) => {
       </div>
       <div className="text-center mt-2 space-y-1">
         <div>{textClip(product.name)}</div>
-        <Rating name="read-only" value={4} readOnly />
+        <Rating name="read-only" value={productRating} readOnly />
         <div className="text-orange-500 font-bold text-lg md:text-xl">
           {product.price} ₺
         </div>
