1b50e054ccc2c7cca7ab0ff8ccd33e2d7b1e6dd9
rikords <iamhakanduyar@gmail.com>
2024-05-29 22:32:31 +0300

    - Initialized router with useRouter hook - Added onClick event to navigate to product detail page using router.push - Applied styling to the product card for better visual presentation

--- app/components/home/ProductCard.tsx
+++ app/components/home/ProductCard.tsx
@@ -1,13 +1,21 @@
+"use client";
+
 import textClip from "@/utils/TextClip";
 import { Rating } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const ProductCard = ({ product }: { product: any }) => {
+  const router = useRouter();
+
   let productRating =
     product?.reviews?.reduce((acc: number, item: any) => acc + item.rating, 0) /
     product?.reviews?.length;
   return (
-    <div className="w-[240px] cursor-pointer flex flex-col flex-1 shadow-lg p-2 rounded-md">
+    <div
+      onClick={() => router.push(`product/${product.id}`)}
+      className="w-[240px] cursor-pointer flex flex-col flex-1 shadow-lg p-2 rounded-md"
+    >
       <div className="relative h-[150px]">
         <Image
           src={product.image}
