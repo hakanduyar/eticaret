7a9c0ca211fe240d587f7f9474e7b869ae9aaf3a
rikords <iamhakanduyar@gmail.com>
2024-05-31 08:37:11 +0300

    - Created DetailClient component - DetailClient is designed to display detailed information about a product - The component includes product image, name, description, and stock status - PageContainer component is used for page layout consistency

--- app/components/detail/DetailClient.tsx
+++ app/components/detail/DetailClient.tsx
@@ -1,6 +1,33 @@
 "use client";
+
+import Image from "next/image";
+import PageContainer from "../containers/PageContainer";
+
 const DetailClient = ({ product }: { product: any }) => {
-  return <div>DetailClient</div>;
+  return (
+    <div className="my-10">
+      <PageContainer>
+        <div className="block md:flex gap-10 justify-center">
+          <div className="relative h-[400px] w-[400px] ">
+            <Image src={product?.image} fill alt="" />
+          </div>
+          <div className="w-1/2 space-y-3">
+            right
+            <div className="text-xl md:text-2xl">{product?.name}</div>
+            <div className="text-slate-500">{product?.description}</div>
+            <div className="flex items-center gap-2">
+              <div>STOK DURUMU:</div>
+              {product.inStock ? (
+                <div className="text-green-500">Ürün Stokta Mevcut</div>
+              ) : (
+                <div className="text-red-500">Ürün Stokta Bulunmamakta</div>
+              )}
+            </div>
+          </div>
+        </div>
+      </PageContainer>
+    </div>
+  );
 };
 
 export default DetailClient;
