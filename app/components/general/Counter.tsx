46fdd83b6131813afe9ba5f1b3c4e19c9d03d317
rikords <iamhakanduyar@gmail.com>
2024-06-04 15:16:48 +0300

    feat: Add Counter component for product quantity management
    - Created Counter component to manage product quantity.
    - Implemented increase and decrease functions in DetailClient component.
    - Integrated Counter component with product state in DetailClient.

---
+++ app/components/general/Counter.tsx
@@ -0,0 +1,23 @@
+import { CardProductProps } from "../detail/DetailClient";
+
+interface CounterProps {
+  cardProduct: CardProductProps;
+  increaseFunc: () => void;
+  decreaseFunc: () => void;
+}
+
+const Counter: React.FC<CounterProps> = ({
+  cardProduct,
+  increaseFunc,
+  decreaseFunc,
+}) => {
+  return (
+    <div>
+      <div onClick={decreaseFunc}>-</div>
+      <div>{cardProduct?.quantity}</div>
+      <div onClick={increaseFunc}>+</div>
+    </div>
+  );
+};
+
+export default Counter;
