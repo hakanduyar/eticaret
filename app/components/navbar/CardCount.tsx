6b62f704b5ae966975d18d9f939642ccdb486bdc
rikords <iamhakanduyar@gmail.com>
2024-06-17 18:19:55 +0300

    Enhance CardCount component with cart product count and basket icon
    - Added "use client" directive for Next.js compatibility.
    - Imported `useCart` hook from "@/hooks/useCart" to access cart products.
    - Imported `SlBasket` icon from "react-icons/sl" for a visual basket representation.
    - Integrated `useCart` hook to fetch the number of products in the cart and display it dynamically.
    - Added a badge showing the count of items in the cart:
      - Positioned the badge at the top-right of the basket icon.
      - Styled the badge with a small, rounded orange background and white text for visibility.
    - Updated the component layout to include:
      - A "Sepetim" label next to the basket icon.
      - The `SlBasket` icon for visual representation of the cart.
    - These changes improve user experience by providing a clear and immediate visual indicator of the number of items in the cart.
    
    No functional changes were made to the components themselves outside of the visual and informational enhancements.

--- app/components/navbar/CardCount.tsx
+++ app/components/navbar/CardCount.tsx
@@ -1,5 +1,18 @@
+"use client";
+import useCart from "@/hooks/useCart";
+import { SlBasket } from "react-icons/sl";
 const CardCount = () => {
-  return <div className="hidden md:flex">CardCount</div>;
+  const { cartPrdcts } = useCart();
+
+  return (
+    <div className="hidden items-center md:flex relative">
+      <div className="absolute top-0 right-0 text-xs bg-orange-500 text-white w-4 h-4 flex items-center justify-center rounded-full transform translate-x-1/2 -translate-y-1/2">
+        {cartPrdcts?.length}
+      </div>
+      <span className="ml-2">Sepetim</span>
+      <SlBasket size={20} className="relative" />
+    </div>
+  );
 };
 
 export default CardCount;
