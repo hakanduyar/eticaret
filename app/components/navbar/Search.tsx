d913d866aeb2bada20f9c7809778b0ca73769edc
rikords <iamhakanduyar@gmail.com>
2024-06-28 17:19:58 +0300

    feat: Implement basic search input component for desktop view
    - Created `Search` component with a responsive design:
      - Utilized Tailwind CSS classes to style the component, ensuring it remains hidden on smaller screens (`hidden md:flex`).
      - Set up a flex container to manage layout (`flex-1`).
    
    - Added a styled input field for search queries:
      - Applied padding (`py-2 px-3`) and a background color (`bg-slate-600`) for visual consistency.
      - Rounded the corners (`rounded-sm`) and removed default borders (`border-none`) and outlines (`outline-none`).
      - Ensured the input field expands to fill available space (`flex flex-1`).
      - Set a placeholder text ("Arama Yap...") to guide users.
    
    - Included a basic search button:
      - Added padding (`p-2`), leaving the background color class incomplete (`bg-`), allowing for future customization.
    
    This commit introduces a basic search bar designed for desktop views, providing the foundational structure and styling needed for a search input field and button.

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,5 +1,14 @@
 const Search = () => {
-  return <div className="hidden md:flex flex-1">Search</div>;
+  return (
+    <div className="hidden md:flex flex-1">
+      <input
+        className="py-2 px-3 bg-slate-600 rounded-sm border-none outline-none flex flex-1"
+        type="text"
+        placeholder="Arama Yap..."
+      />
+      <button className="p-2 bg-">Ara</button>
+    </div>
+  );
 };
 
 export default Search;
