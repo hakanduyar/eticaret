0f95b16202591f334bef025edd0ca901dfe1d6e0
rikords <iamhakanduyar@gmail.com>
2024-07-13 23:35:46 +0300

    Enhance sign-up component with better styling and Google icon integration
    - Added import for "FcGoogle" from "react-icons/fc" to include a Google icon.
    - Modified the container div to include "my-6" for consistent vertical spacing.
    - Updated the onClick handler for the "Kayıt Ol" button to use handleSubmit(onSubmit) for proper form submission handling.
    - Enhanced the "OR" divider with "text-center my-2 font-bold text-lg" classes for better styling and alignment.
    - Added "icon={FcGoogle}" to the "Google ile Üye Ol" button to visually indicate Google sign-up option.
    - These changes improve the overall user interface and provide better visual cues for the sign-up options.

--- app/components/auth/RegisterClient.tsx
+++ app/components/auth/RegisterClient.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
 
 import { register } from "module";
 import AuthContainer from "../containers/AuthContainer";
@@ -19,7 +20,7 @@ const RegisterClient = () => {
 
   return (
     <AuthContainer>
-      <div className="w-full md:w-[500px] p-3 shadow-lg rounded-md">
+      <div className="w-full my-6 md:w-[500px] p-3 shadow-lg rounded-md">
         <Heading text="Register" center />
         <Input
           placeholder="Ad"
@@ -45,9 +46,15 @@ const RegisterClient = () => {
           errors={errors}
           required
         />
-        <Button text="Kayıt Ol" outline onClick={onSubmit()} />
-        <div>OR</div>
-        <Button text="Google ile Üye Ol" outline onClick={() => {}} />
+        <Button text="Kayıt Ol" outline onClick={handleSubmit(onSubmit)} />
+        <div className="text-center my-2 font-bold text-lg">OR</div>
+        
+        <Button
+          text="Google ile Üye Ol"
+          icon={FcGoogle}
+          outline
+          onClick={() => {}}
+        />
       </div>
     </AuthContainer>
   );
