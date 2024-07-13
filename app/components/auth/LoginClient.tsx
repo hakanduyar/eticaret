bffd6cf7e4f175ed995e20165df92f6db3df805f
rikords <iamhakanduyar@gmail.com>
2024-07-13 23:36:20 +0300

    Implement LoginClient component with enhanced form and Google login option
    - Added import for "useForm", "SubmitHandler", and "FieldValues" from "react-hook-form" for form handling.
    - Included import for "FcGoogle" from "react-icons/fc" to add a Google icon to the Google login button.
    - Imported necessary components such as AuthContainer, Heading, Input, and Button.
    - Created the LoginClient component to handle user login.
    - Utilized "useForm" hook to manage form state and validation.
    - Defined onSubmit handler to log form data on submission.
    - Structured the form with Email and Password inputs using the Input component.
    - Added a "Giriş Yap" button with an onClick handler using handleSubmit to trigger form submission.
    - Enhanced the "OR" divider with styling for better visual separation.
    - Included a link to the registration page with a message for new users.
    - Added a "Google ile Giriş Yap" button with an icon for Google login option.
    - These changes provide a complete and user-friendly login form with enhanced styling and functionality, including an option for Google login.

--- app/components/auth/LoginClient.tsx
+++ app/components/auth/LoginClient.tsx
@@ -1,7 +1,61 @@
+"use client";
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+
+import { register } from "module";
 import AuthContainer from "../containers/AuthContainer";
+import Heading from "../general/Heading";
+import Input from "../general/Input";
+import { error } from "console";
+import Button from "../general/Button";
+import Link from "next/link";
 
 const LoginClient = () => {
-  return <AuthContainer>Login</AuthContainer>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FieldValues>();
+  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log(data);
+
+  return (
+    <AuthContainer>
+      <div className="w-full my-6 md:w-[500px] p-3 shadow-lg rounded-md">
+        <Heading text="Login" center />
+        <Input
+          placeholder="Email"
+          type="text"
+          id="email"
+          register={register}
+          errors={errors}
+          required
+        />
+        <Input
+          placeholder="Parola"
+          type="password"
+          id="password"
+          register={register}
+          errors={errors}
+          required
+        />
+        <Button text="Giriş Yap" outline onClick={handleSubmit(onSubmit)} />
+        <div className="text-center my-2 font-bold text-lg">OR</div>
+        <div className="text-center my-2 text-sm">
+          Hala kayıt olmadıysan{" "}
+          <Link className="underline" href="register">
+            buraya tıkla{" "}
+          </Link>
+        </div>
+        <Button
+          text="Google ile Giriş Yap"
+          icon={FcGoogle}
+          outline
+          onClick={() => {}}
+        />
+      </div>
+    </AuthContainer>
+  );
 };
 
 export default LoginClient;
